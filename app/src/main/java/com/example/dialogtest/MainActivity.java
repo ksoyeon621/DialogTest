6c63427b1b6e43b4669a1a3f71ea94f7200d3fe8
ksoyeon621 <ksoyeon621@naver.com>
2017-05-25 20:35:17 +0900

    #2 set Dialog

--- app/src/main/java/com/example/dialogtest/MainActivity.java
+++ app/src/main/java/com/example/dialogtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dialogtest;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);//대화상자가 어느액티비티에서 만들어지는지 지정//getApplicationContext()사용가능
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.drawable.flamingo);
+        dialog.setMessage("여기는 메세지를 쓰는 곳입니다.");
+        dialog.setPositiveButton("Ok", null/*처리자를 넣는곳*/);
+        dialog.show();
     }
 }
